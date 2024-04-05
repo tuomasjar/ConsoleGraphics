01ab57fe81f55b78653a10ee91ec604eb26bb112
tuomasjar <tuomasjar77@gmail.com>
2024-04-05 22:55:52 +0300

    Movement acceleration Collision detection Map to level

--- ConsoleGraphics.cpp
+++ ConsoleGraphics.cpp
@@ -8,12 +8,14 @@ using namespace std;
 
 const int m_screenWidth = 120;
 const int m_screenHeight = 40;
-const int m_mapWidth = 12;
+const int m_mapWidth = 30;
 const int m_mapHeight = 10;
 const float gravity = 9.81;
-const float acceleration = 5;
+const float acceleration = 10;
 const float maxSpeed = 15;
-WCHAR emptyChar = ' ';
+const float jumpSpeed = 15;
+const WCHAR emptyChar = ' ';
+const WCHAR fullBlock = (WCHAR)0xfeff2588;
 auto tp1 = chrono::system_clock::now();
 auto tp2 = chrono::system_clock::now();
 Entity Player;
@@ -21,19 +23,19 @@ Entity Player;
 int main()
 {
     wstring map;
-    map += L"############";
-    map += L"#..........#";
-    map += L"#.......####";
-    map += L"#..........#";
-    map += L"#......##..#";
-    map += L"#......##..#";
-    map += L"#..........#";
-    map += L"###........#";
-    map += L"#......#####";
-    map += L"############";
+    map += L"##############################";
+    map += L"#............................#";
+    map += L"#.........................####";
+    map += L"#......................##....#";
+    map += L"#.................###....##..#";
+    map += L"#...........###..........##..#";
+    map += L"#.....###....................#";
+    map += L"###..........................#";
+    map += L"#........................#####";
+    map += L"##############################";
 
 
-    Player = { 1,17,0,0,false,(WCHAR)0xfeff263A };
+    Player = { 10,17,0,0,false,(WCHAR)0xfeff263A };
     wchar_t* screen = new wchar_t[m_screenWidth * m_screenHeight];
     std::fill_n(screen, m_screenWidth * m_screenHeight, emptyChar);
     HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER,NULL);
@@ -44,34 +46,39 @@ int main()
     SetConsoleCursorInfo(hConsole, &info);
     DWORD dwBytesWritten = 0;
     DWORD dwConSize;
+    for (int x = 0; x < m_screenWidth; x++)
+        for(int y = 0; y < m_screenHeight; y++){
+        if (map[(y / 4)*m_mapWidth + (x/4)] == '#') {
+            screen[y*m_screenWidth + x] = fullBlock;
+        }
+    }
     for (;;) {
         tp2 = chrono::system_clock::now();
         chrono::duration<float> elapsedTime = tp2 - tp1;
         tp1 = tp2;
         float fElapsedTime = elapsedTime.count();
 
-        screen[(short)Player.Y * m_screenWidth + (short)Player.X] = emptyChar;
-
-        /*if (GetAsyncKeyState(VK_UP) & 0x8000) {
-            if (Player.Y > 0) {
-                Player.Y -= 5.0 * fElapsedTime;
-            }
-        }*/
+        screen[(int)Player.Y * m_screenWidth + (int)Player.X] = emptyChar;
+        map[(int)(Player.Y / 4) * m_mapWidth + (int)(Player.X / 4)] = '.';
         
+        
+
         if (GetAsyncKeyState(VK_UP) & 0x8000 && !Player.jump) {
-            Player.ySpeed = -10;
+            Player.ySpeed = -jumpSpeed;
             Player.jump = true;
-        }
-
-        Player.ySpeed += gravity * fElapsedTime;
-        Player.Y += Player.ySpeed * fElapsedTime;
-        
-        if (Player.Y > m_screenHeight /* || map[(int)(Player.Y / 4) * m_mapWidth + (int)Player.X] == '#'*/) {
-            Player.Y -= 1;
+        }else if (Player.Y > m_screenHeight) {
+            Player.Y = m_screenHeight - 1;
+            Player.ySpeed = 0;
+            Player.jump = false;
+        } else if (screen[(int)(Player.Y - 0.5) * m_screenWidth + (int)(Player.X)] == fullBlock) {
+            Player.ySpeed = 0;
+        } else if (screen[(int)(Player.Y + 0.5) * m_screenWidth + (int)(Player.X)] == fullBlock) {
             Player.ySpeed = 0;
             Player.jump = false;
         }
-        
+        Player.ySpeed += gravity * fElapsedTime;
+        Player.Y += Player.ySpeed * fElapsedTime;
+
         if (GetAsyncKeyState(VK_LEFT) & 0x8000) {
             if (Player.xSpeed > -maxSpeed) {
                 Player.xSpeed -= acceleration * fElapsedTime;
@@ -88,6 +95,17 @@ int main()
                 Player.xSpeed = maxSpeed;
             }
         }
+        if (GetAsyncKeyState(VK_DOWN) & 0x8000) {
+            if (Player.xSpeed < 0) {
+                Player.xSpeed += acceleration * fElapsedTime;
+            }
+            if (Player.xSpeed > 0) {
+                Player.xSpeed -= acceleration * fElapsedTime;
+            }
+            if (abs(Player.xSpeed) < 0.1) {
+                Player.xSpeed = 0;
+            }
+        }
         Player.X += Player.xSpeed * fElapsedTime;
         if (Player.X < 0) {
             Player.X = 0;
@@ -97,15 +115,20 @@ int main()
             Player.X = m_screenWidth - 1;
             Player.xSpeed = 0;
         }
+        if (Player.xSpeed < 0 && screen[(int)Player.Y * m_screenWidth + (int)(Player.X - 1)] == fullBlock) {
+            Player.xSpeed = 0;
+        } else if (Player.xSpeed > 0 && screen[(int)Player.Y * m_screenWidth + (int)(Player.X + 1)] == fullBlock) {
+            Player.xSpeed = 0;
+        }
         if (GetAsyncKeyState((unsigned short)'Q') & 0x8000) {
             return 0;
         }
-
+        map[(int)(Player.Y / 4) * m_mapWidth + (int)(Player.X / 4)] = Player.symbol;
         for (int nx = 0; nx < m_mapWidth; nx++)
             for (int ny = 0; ny < m_mapHeight; ny++) {
                 screen[(ny+1) * m_screenWidth + nx] = map[ny * m_mapWidth + nx];
             }
-        swprintf_s(screen, 40, L"X=%3.2f, Y=%3.2f, FPS=%3.2f ", Player.xSpeed, Player.ySpeed, 1.0f / fElapsedTime);
+        swprintf_s(screen, 60, L"X=%d, Y=%d, xSpeed = %3.2f, ySpeed = %3.2f, FPS=%3.2f ", (int)Player.X, (int)Player.Y, Player.xSpeed, Player.ySpeed, 1.0f / fElapsedTime);
         screen[(short)Player.Y * m_screenWidth + (short)Player.X] = Player.symbol;
         screen[m_screenWidth * m_screenHeight - 1] = '\0';
         
